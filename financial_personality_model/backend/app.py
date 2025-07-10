a54cb5c2514318fdd74bbdad405d880ba807a8aa
GUO XIAOYUE <1776614645@qq.com>
2025-07-10 22:10:11 +0800

    Add files via upload

--- financial_personality_model/backend/app.py
+++ financial_personality_model/backend/app.py
@@ -35,6 +35,7 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 1000))) 
+    port = int(os.environ.get("PORT", 5000))  # 从环境变量获取 PORT，默认 5000
+    app.run(host="0.0.0.0", port=port)
 
 
